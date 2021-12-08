730925c998bf1047199dceaf1cd8e9c7714c5ee5
XAYRGA <dzbush25@gmail.com>
2021-12-08 05:07:57 -0500

    init

---
+++ PainTrainStation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PainTrainStation
+{
+    public static class PainTrainStation
+    {
+        private static int delayTime = 259200;
+        public static long groupID = 0;
+        public static void Enter()
+        {
+            while (true)
+            {
+                processUpdates();
+                Thread.Sleep(50);
+            }
+        }
+
+        static long lastUpdate = 0;
+        public static void processUpdates()
+        {
+            var up = Telegram.getUpdates(lastUpdate);
+            if (up == null)
+            {
+                Console.WriteLine("TGAPI Response failure update==null");
+                return;
+            }
+            Console.WriteLine("Updates: {0}", up.Length);
+
+            for (int i = 0; i < up.Length; i++)
+            {
+                var currentUpdate = up[i];
+                if (currentUpdate.update_id >= lastUpdate)
+                {
+                    lastUpdate = currentUpdate.update_id + 1;
+                }
+                if (currentUpdate.edited_message != null)
+                {
+                    currentUpdate.message = currentUpdate.edited_message; // ahax.
+                }
+                //Console.WriteLine(JsonConvert.SerializeObject(currentUpdate));
+                {
+                    if (currentUpdate.message != null)
+                    {
+                        processIndividualUpdate(currentUpdate);
+                    }
+                }
+            }
+        }
+        
+   
+
+
+        public static void processIndividualUpdate(TGUpdate update)
+        {
+            var msg = update.message;
+            if (msg.from.is_bot)
+                return;
+            if (msg.from == null)
+                return;
+            if (msg.from.id == 777000)
+                msg.deleteAsync();
+        }
+    }
+}
+
---
+++ root.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace PainTrainStation
+{
+    class root
+    {
+        private const string tag = "PainTrainStation@boot";
+        public static string botUsername;
+        public static string botName;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PainTrainStation- 12-8-2021");
+            // Param check 
+            if (args.Length > 0)
+            {
+                var ptg = "PainTrainStation@preboot";
+                Console.WriteLine("parameter check.....");
+            }
+            Helpers.writeOut(tag, "Initializing configuration.");
+            Config.init("config.ini");
+            Telegram.SetAPIKey(Config.getValue("TGAPIKey"));
+            {
+                var tries = 0;
+                var me = Telegram.getMe(); // Synchronous call for result.
+                while (me == null)
+                {
+                    tries++;
+                    Thread.Sleep(1200);
+                    Helpers.warn("Failed. Trying again");
+                    me = Telegram.getMe();
+                    if (tries > 3)
+                    {
+                        Helpers.warn("Invalid telegram API key or cannot connect to tgapi.");
+                        Environment.Exit(-1);
+                    }
+                }
+                botUsername = me.username; // set bot username
+                botName = me.first_name; // set bot name
+                Console.WriteLine($"Hello, I'm {botName} under the handle {botUsername}");
+            }
+
+
+            PainTrainStation.Enter();
+        }
+    }
+}
